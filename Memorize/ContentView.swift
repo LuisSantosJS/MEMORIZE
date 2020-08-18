706be5913df18179cb5e28603137690f3652c9c9
Luis Santos <dasilvasantosluisfelipe@gmail.com>
2020-08-17 21:52:58 -0500

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Luis Santos on 17/08/20.
+//  Copyright © 2020 Luis Santos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
